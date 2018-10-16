f7e64450813b76e7e3299fc3c077a992185cec8a
ksh <openshg@gmail.com>
2018-10-16 14:11:46 +0900

    CSV, Json 예제 완료.

---
+++ CSV_Json_Sample/Assets/JsonEx/JsonUtilEx.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace JsonEx
+{
+    public static class JsonUtilEx
+    {
+        public static T LoadAndDeserialize<T>(string path, string fileName)
+        {
+            //string filePath = string.Format(path, Application.dataPath);
+
+            string JsonFileName = string.Format("{0}/{1}", path, fileName);
+
+            var streamReader = new System.IO.StreamReader(JsonFileName);
+            string data = streamReader.ReadToEnd();
+            streamReader.Close();
+
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+
+        public static void SaveJsonObject<T>(T obj, string _path, string _filename)
+        {
+            string JsonFileName = string.Format("{0}/{1}", _path, _filename);
+
+            StreamWriter stream_write;
+            stream_write = File.CreateText(JsonFileName);
+
+            string jsonString = JsonConvert.SerializeObject(obj, Newtonsoft.Json.Formatting.Indented);
+
+            stream_write.Write(jsonString);
+
+#if UNITY_EDITOR
+            Application.OpenURL(_path);
+#endif
+            stream_write.Close();
+        }
+    }
+}
---
+++ CSV_Json_Sample/Assets/TestCode/JsonSample.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.IO;
+using Newtonsoft.Json;
+
+using JsonEx;
+
+public class TestJsonData
+{
+    public int Key;
+    public string str01;
+    public string str02;
+    public List<string> lst_str;
+}
+
+public class JsonSample : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Dictionary<int, TestJsonData> temp = new Dictionary<int, TestJsonData>();
+
+        for(int i=0; i<10;++i)
+        {
+            TestJsonData ddd = new TestJsonData();
+
+            ddd.lst_str = new List<string>();
+
+            ddd.Key = i;
+            ddd.str01 = i.ToString();
+            ddd.str02 = i.ToString();
+
+            for(int j=0; j<4; ++j)
+            {
+                ddd.lst_str.Add(j.ToString());
+            }
+
+            temp.Add(ddd.Key, ddd);
+        }
+
+        JsonUtilEx.SaveJsonObject(temp, Application.persistentDataPath, "TEST_JSON");
+
+        Dictionary<int, TestJsonData> tem = JsonUtilEx.LoadAndDeserialize<Dictionary<int, TestJsonData>>(Application.persistentDataPath, "TEST_JSON");
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void MakeTestJson()
+    {
+
+    }
+}
